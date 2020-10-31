f92c334db2400a3e60b88c9d86d2e7134d088e95
rabeet-k1 <rabeet.k1@gmail.com>
2020-10-31 10:57:00 +0500

    first commit

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
-
+ 
 const NavBar = ({ user }) => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
